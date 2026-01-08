d71bf1e759ecaa9f3ded8769137eb4eb8dcccec3
atarsander <szymczyk.alek1@gmail.com>
2026-01-08 21:13:56 +0100

    Added reranking logic

--- src/rag/retrieve/retriever.py
+++ src/rag/retrieve/retriever.py
@@ -1,26 +1,32 @@
 import numpy as np
+import torch
+from qdrant_client import QdrantClient
 from qdrant_client.http import models as qm
-from sentence_transformers import SentenceTransformer
+from sentence_transformers import SentenceTransformer, CrossEncoder
 
-from rag.config import EMBEDDER
+from rag.config import EMBEDDER, RERANKER
 
 
 class Retriever:
-    def __init__(self, client, collection_name: str):
+    def __init__(self, client: QdrantClient, collection_name: str):
         self.client = client
         self.collection_name = collection_name
         self.embedder = SentenceTransformer(EMBEDDER)
+        self.reranker = CrossEncoder(RERANKER, device="cpu")
 
     def retrieve_table_and_paragraphs(
         self,
         query: str,
+        top_n: int = 10,
+        top_k: int = 7,
     ):
         query_vec = self.embedd_query(query)
 
         table_filter = qm.Filter(
             must=[qm.FieldCondition(key="type", match=qm.MatchValue(value="table"))]
         )
-        table_hits = self.search(query_vec, filter=table_filter)
+        table_hits = self.search(query_vec, filter=table_filter, k=top_n)
+        table_hits = self.rerank(query, hits=table_hits, top_k=top_k)
 
         context_ids = []
 
@@ -35,10 +41,12 @@ class Retriever:
             ]
         )
 
-        para_hits = self.search(query_vec, filter=para_filter)
+        para_hits = self.search(query_vec, filter=para_filter, k=top_n)
+        para_hits = self.rerank(query, hits=para_hits, top_k=top_k)
+
         return table_hits, para_hits
 
-    def search(self, query_vec, k: int = 10, filter=None):
+    def search(self, query_vec, k: int, filter=None):
         response = self.client.query_points(
             collection_name=self.collection_name,
             query=query_vec,
@@ -48,5 +56,27 @@ class Retriever:
         )
         return response.points
 
+    def rerank(self, query, hits, top_k: int):
+        candidates = []
+        for hit in hits:
+            if "text" in hit.payload.keys():
+                content = hit.payload["text"]
+            else:
+                content = hit.payload["table"]
+            candidates.append(content)
+
+        pairs = [[query, doc] for doc in candidates]
+        rerank_scores = self.reranker.predict(pairs)
+
+        ranked = [
+            h
+            for _, h in sorted(
+                zip(rerank_scores, hits), key=lambda x: x[0], reverse=True
+            )
+        ]
+
+        return ranked[:top_k]
+
     def embedd_query(self, query: str) -> np.ndarray:
         return self.embedder.encode(query)
+
